7cbfb4aec066cdef6641eb5da8a3a7ef7411b2af
Aron Jones <aron.jones@gmail.com>
2018-06-09 11:58:48 -0400

    prod settings

--- spendwell/settings/production.py
+++ spendwell/settings/production.py
@@ -1,5 +1,5 @@
 
-import raven
+™import raven
 
 from .base import *
 from .secrets import db_password, raven_dsn, mailgun_smtp_password
@@ -8,40 +8,42 @@ from .secrets import db_password, raven_dsn, mailgun_smtp_password
 SITE_DOMAIN = 'www.spendwell.co'
 ALLOWED_HOSTS = [SITE_DOMAIN]
 
-DATABASES['default'] = {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'HOST': '172.16.87.148',
-    'NAME': 'spendwell',
-    'USER': 'spendwell',
-    'PASSWORD': db_password,
-}
+django_heroku.settings(locals())
 
-INSTALLED_APPS.append('raven.contrib.django.raven_compat')
+# DATABASES['default'] = {
+#     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#     'HOST': '172.16.87.148',
+#     'NAME': 'spendwell',
+#     'USER': 'spendwell',
+#     'PASSWORD': db_password,
+# }
 
-MIDDLEWARE_CLASSES.append(
-    'raven.contrib.django.raven_compat.middleware.SentryResponseErrorIdMiddleware'
-)
+# INSTALLED_APPS.append('raven.contrib.django.raven_compat')
 
-
-CACHES['default']['LOCATION'] = 'redis://172.16.87.154:6379/1'
-BROKER_URL = 'redis://172.16.87.154:6379/0'
+# MIDDLEWARE_CLASSES.append(
+#     'raven.contrib.django.raven_compat.middleware.SentryResponseErrorIdMiddleware'
+# )
 
 
-LOGGING['handlers']['sentry'] = {
-    'level': 'ERROR',
-    'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
-}
-LOGGING['loggers']['django']['handlers'] = ['console', 'sentry']
+# CACHES['default']['LOCATION'] = 'redis://172.16.87.154:6379/1'
+# BROKER_URL = 'redis://172.16.87.154:6379/0'
+
+
+# LOGGING['handlers']['sentry'] = {
+#     'level': 'ERROR',
+#     'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+# }
+# LOGGING['loggers']['django']['handlers'] = ['console', 'sentry']
 
 
 EMAIL_HOST_PASSWORD = mailgun_smtp_password
 
 
-RAVEN_PUBLIC_DSN = 'https://e82e41c7ae084a72b64d0571f6b4dcfd@app.getsentry.com/73495'
-RAVEN_CONFIG = {
-    'dsn': raven_dsn,
-    'release': raven.fetch_git_sha(BASE_DIR),
-}
+# RAVEN_PUBLIC_DSN = 'https://e82e41c7ae084a72b64d0571f6b4dcfd@app.getsentry.com/73495'
+# RAVEN_CONFIG = {
+#     'dsn': raven_dsn,
+#     'release': raven.fetch_git_sha(BASE_DIR),
+# }
 
 INCLUDE_ANALYTICS = True
 MIXPANEL_PUBLIC_KEY = '25e4cbc88d444229cfcddfe21935ed2c'
